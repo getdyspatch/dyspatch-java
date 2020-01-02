142aae6927e081203388d281802e6863fc837cb0
Colin Walker <colin@sendwithus.com>
2020-01-02 14:18:01 -0800

    Implement CI

---
+++ src/test/java/io/dyspatch/client/api/IntegrationTest.java
@@ -0,0 +1,47 @@
+package io.dyspatch.client.api;
+
+import io.dyspatch.client.ApiClient;
+import io.dyspatch.client.ApiException;
+import io.dyspatch.client.Configuration;
+import io.dyspatch.client.auth.*;
+import io.dyspatch.client.api.DraftsApi;
+import io.dyspatch.client.api.TemplatesApi;
+
+import org.junit.Test;
+
+/**
+ * Integration tests.
+ * THESE ARE HAND BUILT AND NOT GENERATED, DO NOT DELETE
+ */
+public class IntegrationTest {
+    String version = "application/vnd.dyspatch.2019.10+json";
+    ApiClient client;
+    DraftsApi drafts;
+    TemplatesApi templates;
+    ApiKeyAuth auth;
+
+    public IntegrationTest() {
+        client = Configuration.getDefaultApiClient();
+        ApiKeyAuth auth = (ApiKeyAuth) client.getAuthentication("Bearer");
+        auth.setApiKey(System.getenv("DYSPATCH_API_KEY"));
+
+        templates = new TemplatesApi(client);
+        drafts = new DraftsApi(client);
+    }
+
+    @Test
+    public void getTemplates() throws ApiException {
+        templates.getTemplates(version, "");
+
+        String templateId = "tem_01de5teh6k59kya8q92mb01qzq";
+        templates.getTemplateById(templateId, "handlebars", version);
+    }
+
+    @Test
+    public void getDrafts() throws ApiException {
+        drafts.getDrafts(version, "", "");
+
+        String draftId = "tdft_01dxkwr0nevs5h2baa3n3dgktp";
+        drafts.getDraftById(draftId, "handlebars", version);
+    }
+}
