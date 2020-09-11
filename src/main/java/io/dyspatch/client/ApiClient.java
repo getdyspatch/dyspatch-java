f0492f5f4842233a4dc0084605582acd716664d9
Daniel Steuernol <dlsteuer@gmail.com>
2020-09-11 10:05:36 -0700

    Generate 2020.08

--- src/main/java/io/dyspatch/client/ApiClient.java
+++ src/main/java/io/dyspatch/client/ApiClient.java
@@ -109,7 +109,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/5.0.2/java");
+        setUserAgent("OpenAPI-Generator/6.0.0/java");
 
         authentications = new HashMap<String, Authentication>();
     }
